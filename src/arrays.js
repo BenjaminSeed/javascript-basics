0aab5dd9deb376b376a39a98b502271e4d51d917
Benjamin <benseed.bs@gmail.com>
2022-07-28 22:21:46 +0100

    First 9 Array tests

--- src/arrays.js
+++ src/arrays.js
@@ -1,33 +1,35 @@
 const getNthElement = (index, array) => {
-  // your code here
-};
+  return array[index % array.length]
+}
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString()
 };
 
-const csvStringToArray = string => {
-  // your code here
-};
+function csvStringToArray(string) {
+  return string.split(',')
+}
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element)
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return array.concat(element)
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1)
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(number => number.toString())
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(element => {
+    return element.toUpperCase()
+  })
 };
 
 const reverseWordsInArray = strings => {
